3858b306ad33fc6272791b046e385311577b462c
hoshito <tweicomepan350@gmail.com>
2019-01-19 22:09:03 +0900

    add markdown component

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,4 +1,14 @@
 module.exports = {
-  plugins: [`gatsby-plugin-netlify-cms`],
+  plugins: [
+  {
+    resolve: `gatsby-source-filesystem`,
+    options: {
+      path: `${__dirname}/blog`,
+      name: "markdown-pages",
+    },
+  },
+  `gatsby-transformer-remark`,
+  `gatsby-plugin-netlify-cms`
+  ],
 }
 
